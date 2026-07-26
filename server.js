dbb3065986f8cf6ca8b1ec84a6bb3c9d53060526
unknown <aitools012@gmail.com>
2026-07-26 15:06:28 +0530

    Fix Express 5 wildcard routing crash

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ setupGameLogic(io);
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Handle React routing, return all requests to React app
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
